12ebe80b5c98c349e5052e25b23637fd9b1ce24b
aravindkv27 <aravind27032002@gmail.com>
2023-01-06 22:13:00 +0530

    Login Authentication completed

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,11 @@
 import { HttpClient, HttpResponseBase } from '@angular/common/http';
 import { ReadPropExpr } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Route,Router } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { ProserviceService } from '../proservice.service';
 // import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import { MatSnackBar } from '@angular/material/snack-bar';
 // import { Route,Router } from '@angular/router';
@@ -13,7 +17,42 @@ import { FormGroup } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   register:any=FormGroup;
   url1 = 'http://localhost:5000/api/register'
+
+  constructor(private http: HttpClient,private fb:FormBuilder,private route: Router,private ps:ProserviceService,private snb:MatSnackBar) { }
+  
+  ngOnInit(): void {
+    this.register=this.fb.group(
+      {
+        email:['',Validators.compose([Validators.required,Validators.email])],
+        pass:['',Validators.compose([Validators.required,Validators.minLength(5)])],
+        repass:['',Validators.compose([Validators.required,Validators.minLength(5)])]
+      }
+    )
+  }
+ 
+  onsubmit(data: any)
+  {
+
+   let params={
+    email:data.email,
+    pass:data.pass,
+   }
+   if(data.pass==data.repass)
+   {
+    this.http.post(this.url1,params).subscribe((data:any)=>{});
+    this.snb.open(' Registered Successfully........Proceed Login ', 'close', {duration: 3000});
+    this.route.navigate(['login'])
+   }
+   else{
+    this.snb.open('Password Mismatch','close',{duration: 2000});
+   }
+  }
+  goToLogin()
+  {
+-
+   this.route.navigate(['login']);
+  }
 }
